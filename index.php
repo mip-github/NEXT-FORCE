823b21752ae364b1a5048e46ba0b5e6b39c351db
CHINJUNGGGG <chinjungworking@gmail.com>
2020-05-11 11:07:00 +0700

    Update index

--- index.php
+++ index.php
@@ -168,61 +168,343 @@ div.single-input  {
     </header>
     <!-- header-end -->
 
-    <!-- slider_area_start -->
     <div class="slider_area">
         <div class="single_slider  d-flex align-items-center slider_bg_1">
             <div class="container">
                 <div class="row align-items-center justify-content-center">
-                    <!-- <div class="col-lg-7 col-md-6">
+                    <div class="col-lg-7 col-md-6">
                         <div class="slider_text">
-                            <h3 class="wow fadeInRight" data-wow-duration="1s" data-wow-delay=".1s">Get Loan for your Business growth or startup</h3>
+                            <h1 class="font02">T10 Cryptocurrency is</h1>
+							<h2 class="font03">cryptocurrency community for business</h2></br>
+							<h4 class="font01">ระบบการเงินโดยเทคโนโลยี Blockchain ที่จะทำให้ทุกการใช้จ่ายในชีวิตประจำวันเป็นเรื่องง่ายผ่านแอปพลิเคชั่น T10 พร้อมทั้งสิทธิ์การสะสม Point ที่สามารถใช้งานควบคู่กับเงินสด และรองรับเฉพาะกลุ่มสมาชิก T10 เท่านั้น</h4>
                             <div class="sldier_btn wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".2s">
-                                <a href="#" class="boxed-btn3">How it Works</a>
+                                <a href="#" class="boxed-btn5"><img src='pic/icon02.png'> White papers</a>&nbsp;&nbsp;
+								<a href="#" class="boxed-btn3"><img src='pic/icon01.png'> Presentation</img></a>
                             </div>
                         </div>
-                    </div> -->
-                    <!-- <div class="col-lg-5 col-md-6">
-                        <div class="payment_form white-bg wow fadeInDown" data-wow-duration="1.2s" data-wow-delay=".2s">
-                            <div class="info text-center">
-                                <h4>How much do you want?</h4>
-                                <p>We provide online instant cash loans with quick</p>
+                    </div>
+                    <div class="col-lg-5 col-md-6">
+                        <div class="boxed-box01">
+                                <div class="font04">Kalungka CIO Finishes in:</div></br>
+									<script src="js/Countdown.js"></script>
+
+                            <div>
+                                <button class="btn6" type="submit">Buy coin 15% off</button></br></br>
                             </div>
-                            <div class="form">
-                                <div class="row">
-                                    <div class="col-lg-12">
-                                        <div class="single_input">
-                                            <select class="wide">
-                                                <option data-display="Amount">Amount</option>
-                                                <option value="1">$10</option>
-                                                <option value="2">$40</option>
-                                                <option value="3">$50</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-12">
-                                        <div class="single_input">
-                                            <select class="wide">
-                                                <option data-display="Month">Month</option>
-                                                <option value="1">3 Month</option>
-                                                <option value="2">6 Month</option>
-                                                <option value="3">9 Month</option>
-                                                <option value="4">12 Month</option>
-                                            </select>
-                                         </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <p>You have to pay: <span>$0</span></p>
-                            <div class="submit_btn">
-                                <button class="boxed-btn3" type="submit">Continue</button>
-                            </div>
+							
+							<div class="p2">Fixed token edition :</div>
+							<div class="p1">We Accepted </div>
+							
+							<div> 
+							<img width='60' height='30' src='pic/Icon_ICO-05.png'> 
+							<img width='60' height='30' src='pic/Icon_ICO-06.png'>
+							<img width='60' height='30' src='pic/Icon_ICO-07.png'>
+							<img width='60' height='30' src='pic/Icon_ICO-08.png'>
+							</div>
+							
                         </div>
-                    </div> -->
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+    <div align = 'center'>
+		<div class="font05"><img width='10' height='10' src ='pic/icon03.png'></img> About us</div>
+		<h1 class="font02">Kalungka Project</h1></br>
+		<div class="font06">เป็นโครงการที่เกิดขึ้นจากแรงบันดาลใจของอาจารย์กวง ด้วยความมุ่งมั่นในการรวบรวมชาติพันธ์ต่างๆ ที่อาศัยอยู่ในโลกใบนี้</div>
+		<div class="font06">ได้มีโอกาสมาร่วมและสร้างสรรค์สังคมที่ดีงาม โดยใช้ชื่อ KALUNGKA กาลังกา ที่มีความหมายถึงความเป็นชาติพันธ์ </div>
+		<div class="font06">มีวัฒนธรรมอย่างใหม่เป็นชนเผ่าที่ เกิดขึ้นในศตวรรษที่ 21 และวัฒธรรมนี้จะถูกสร้างสรรค์เป็น Community</div>
+		<div class="font06">หรือวัฒนธรรมชุมชนในแบบที่เป็นของตัวเอง เป็นวิถีของกลุ่มที่ไม่เคยมีมาก่อนโดยรวบรวมวัฒนธรรม ไม่ว่างจะเป็นด้านดนตรี </div>
+		<div class="font06">การเต้นรำ ภาพยนตร์ เสื้อผ้าเครื่องแต่งกายและการกิน การทำอาหาร เป็นชุมชนที่มาให้ความสุข ความสนุกสนาน อย่างกาลังกา </div>
+		<div class="font06">ในลักษณะธรรมชาตินิยมผสมกลมกลืนกับความเป็นไปของเทคโนโลยี Blockchain</div>
+		<div class="font06">มีความทันสมัยแต่ไม่ทิ้งขนบธรรมเนียบประเพณี</div></br></br>
+	
+	<img width='1250 px' src ='pic/pic01.png'></img></br></br></br>
+	<div class="team">
+		<div class="font05"><img width='10' height='10' src ='pic/icon03.png'></img> Investment</div>
+		<h1 class="font02">Investment & Cost Estimation</h1></br>
+		<div class="font06">ความสามารถในการสร้างรายได้ที่หลากหลาายรูปแบบจาก Business Model ที่สามารถสร้าง Customer Segment,</div>
+		<div class="font06">Value Proposition, Channels และ Customer Relationship ที่เฉพาะตัว</div>
+		<div class="font06">รายได้หลักจะถูกแบ่งการลงทุนออกเป็นสัดส่วนการลงทุนในรูปแบบที่ต่างกัน โดนมีกลุ่มหลักดังนี้</div>
+		<div class="font06">ค่าใช้จ่ายในการปรับปรุงสถานที่ ค่าใช้จ่ายในการบริหารจัดการ ค่่าโฆษณาประชาสัมพันธ์ และเงินทุนสำรอง</div>
+        <img width='1250 px' src ='pic/pic02.png'></img></br></br></br>
+        <img width='1250 px' src ='pic/pic03.png'></img></br>
+    </div>
+    </br></br>
+	    <div class="font05"><img width='10' height='10' src ='pic/icon03.png'></img> Team</div>
+		<h1 class="font02">Awesome Team</h1></br>
 
+    <div class="container">
+    	<div class="row text-center">
+    			
+    		<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+    				   
+    				   	<img class="img-rounded" alt="team-photo" src='pic/pe08.jpg' width="100%"> 
+    				   	
+    				   	<div class="team-member"></br>
+                        
+    				   	<h4>John Doe</h4>
+    				   	
+    				   	<p>Managing Director</p>
+                        
+    				   	</div>
+    				   	
+    				   	<p class="social">
+    				   		<a href="#"><span class="fa fa-facebook-square"></span></a>
+    				   		<a href="#"><span class="fa fa-twitter-square"></span></a>
+    				   		<a href="#"><span class="fa fa-linkedin-square"></span></a>
+    				   		<a href="#"><span class="fa fa-google-plus-square"></span></a>
+    				   	</p>
+    						    					    				
+    		 </div> <!--col-lg-4 -->
+    				
+    		 <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+    				   
+    				   	<img class="img-rounded" alt="team-photo" src='pic/pe05.jpg' width="100%">
+    				   	
+    				   	<div class="team-member"></br>
+    				   	<h4>Peter</h4>
+    				   	
+    				   	<p>Project Manager</p>
+                        
+    				   	</div>
+    				   	
+    				   	<p class="social">
+    				   		<a href="#"><span class="fa fa-facebook-square"></span></a>
+    				   		<a href="#"><span class="fa fa-twitter-square"></span></a>
+    				   		<a href="#"><span class="fa fa-linkedin-square"></span></a>
+    				   		<a href="#"><span class="fa fa-google-plus-square"></span></a>
+    				   	</p>
+    						    					    				
+    		 </div> <!--col-lg-4 -->
+    				
+    		 <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+    				   
+    				   	<img class="img-rounded" alt="team-photo" src='pic/pe10.jpg' width="100%"> 
+    				   	
+    				   	<div class="team-member"></br>
+                        
+    				   	<h4>Klara</h4>
+    				   	
+    				   	<p>Syatem Analysis</p>
+                        
+    				   	</div>
+    				   	
+    				   	<p class="social">
+    				   		<a href="#"><span class="fa fa-facebook-square"></span></a>
+    				   		<a href="#"><span class="fa fa-twitter-square"></span></a>
+    				   		<a href="#"><span class="fa fa-linkedin-square"></span></a>
+    				   		<a href="#"><span class="fa fa-google-plus-square"></span></a>
+    				   	</p>
+    						    					    				
+    		    </div> <!-- col-lg-4 -->
+				<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+    				   
+    				   	<img class="img-rounded" alt="team-photo" src='pic/pe06.jpg' width="100%"> 
+    				   	
+    				   	<div class="team-member"></br>
+                        
+    				   	<h4>Anna</h4>
+    				   	
+    				   	<p>Syatem Analysis</p>
+                        
+    				   	</div>
+    				   	
+    				   	<p class="social">
+    				   		<a href="#"><span class="fa fa-facebook-square"></span></a>
+    				   		<a href="#"><span class="fa fa-twitter-square"></span></a>
+    				   		<a href="#"><span class="fa fa-linkedin-square"></span></a>
+    				   		<a href="#"><span class="fa fa-google-plus-square"></span></a>
+    				   	</p>
+    						    					    				
+    		    </div> <!-- col-lg-4 -->
+                   
+    	</div>  <!-- row text-center -->
+    			</br></br>
+				<div class="row text-center">
+    			
+    		<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+    				   
+    				   	<img class="img-rounded" alt="team-photo" src='pic/pe01.jpg' width="100%"> 
+    				   	
+    				   	<div class="team-member"></br>
+                        
+    				   	<h4>Danel</h4>
+    				   	
+    				   	<p>Web Developer</p>
+                        
+    				   	</div>
+    				   	
+    				   	<p class="social">
+    				   		<a href="#"><span class="fa fa-facebook-square"></span></a>
+    				   		<a href="#"><span class="fa fa-twitter-square"></span></a>
+    				   		<a href="#"><span class="fa fa-linkedin-square"></span></a>
+    				   		<a href="#"><span class="fa fa-google-plus-square"></span></a>
+    				   	</p>
+    						    					    				
+    		 </div> <!--col-lg-4 -->
+    				
+    		 <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+    				   
+    				   	<img class="img-rounded" alt="team-photo" src='pic/pe02.jpg' width="100%">
+    				   	
+    				   	<div class="team-member"></br>
+                        
+    				   	<h4>Ethan</h4>
+    				   	
+    				   	<p>Programmer</p>
+                        
+    				   	</div>
+    				   	
+    				   	<p class="social">
+    				   		<a href="#"><span class="fa fa-facebook-square"></span></a>
+    				   		<a href="#"><span class="fa fa-twitter-square"></span></a>
+    				   		<a href="#"><span class="fa fa-linkedin-square"></span></a>
+    				   		<a href="#"><span class="fa fa-google-plus-square"></span></a>
+    				   	</p>
+    						    					    				
+    		 </div> <!--col-lg-4 -->
+    				
+    		 <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+    				   
+    				   	<img class="img-rounded" alt="team-photo" src='pic/pe03.jpg' width="100%"> 
+    				   	
+    				   	<div class="team-member"></br>
+                        
+    				   	<h4>Camilla</h4>
+    				   	
+    				   	<p>Programmer</p>
+                        
+    				   	</div>
+    				   	
+    				   	<p class="social">
+    				   		<a href="#"><span class="fa fa-facebook-square"></span></a>
+    				   		<a href="#"><span class="fa fa-twitter-square"></span></a>
+    				   		<a href="#"><span class="fa fa-linkedin-square"></span></a>
+    				   		<a href="#"><span class="fa fa-google-plus-square"></span></a>
+    				   	</p>
+    						    					    				
+    		    </div> <!-- col-lg-4 -->
+				<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+    				   
+    				   	<img class="img-rounded" alt="team-photo" src='pic/pe09.jpg' width="100%"> 
+    				   	
+    				   	<div class="team-member"></br>
+                        
+    				   	<h4>Vinci</h4>
+    				   	
+    				   	<p>Programmer</p>
+                        
+    				   	</div>
+    				   	
+    				   	<p class="social">
+    				   		<a href="#"><span class="fa fa-facebook-square"></span></a>
+    				   		<a href="#"><span class="fa fa-twitter-square"></span></a>
+    				   		<a href="#"><span class="fa fa-linkedin-square"></span></a>
+    				   		<a href="#"><span class="fa fa-google-plus-square"></span></a>
+    				   	</p>
+    						    					    				
+    		    </div> <!-- col-lg-4 -->
+                   
+    	</div>  <!-- row text-center -->
+		
+		    			</br></br>
+				<div class="row text-center">
+    			
+    		<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+    				   
+    				   	<img class="img-rounded" alt="team-photo" src='pic/pe11.jpg' width="100%"> 
+    				   	
+    				   	<div class="team-member"></br>
+                        
+    				   	<h4>Eva</h4>
+    				   	
+    				   	<p>UX/UI</p>
+                        
+    				   	</div>
+    				   	
+    				   	<p class="social">
+    				   		<a href="#"><span class="fa fa-facebook-square"></span></a>
+    				   		<a href="#"><span class="fa fa-twitter-square"></span></a>
+    				   		<a href="#"><span class="fa fa-linkedin-square"></span></a>
+    				   		<a href="#"><span class="fa fa-google-plus-square"></span></a>
+    				   	</p>
+    						    					    				
+    		 </div> <!--col-lg-4 -->
+    				
+    		 <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+    				   
+    				   	<img class="img-rounded" alt="team-photo" src='pic/pe12.jpg' width="100%">
+    				   	
+    				   	<div class="team-member"></br>
+                        
+    				   	<h4>Isabella</h4>
+    				   	
+    				   	<p>UX/UI</p>
+                        
+    				   	</div>
+    				   	
+    				   	<p class="social">
+    				   		<a href="#"><span class="fa fa-facebook-square"></span></a>
+    				   		<a href="#"><span class="fa fa-twitter-square"></span></a>
+    				   		<a href="#"><span class="fa fa-linkedin-square"></span></a>
+    				   		<a href="#"><span class="fa fa-google-plus-square"></span></a>
+    				   	</p>
+    						    					    				
+    		 </div> <!--col-lg-4 -->
+    				
+    		 <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+    				   
+    				   	<img class="img-rounded" alt="team-photo" src='pic/pe07.jpg' width="100%"> 
+    				   	
+    				   	<div class="team-member"></br>
+                        
+    				   	<h4>Gracie</h4>
+    				   	
+    				   	<p>Admin</p>
+                        
+    				   	</div>
+    				   	
+    				   	<p class="social">
+    				   		<a href="#"><span class="fa fa-facebook-square"></span></a>
+    				   		<a href="#"><span class="fa fa-twitter-square"></span></a>
+    				   		<a href="#"><span class="fa fa-linkedin-square"></span></a>
+    				   		<a href="#"><span class="fa fa-google-plus-square"></span></a>
+    				   	</p>
+    						    					    				
+    		    </div> <!-- col-lg-4 -->
+				<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+    				   
+    				   	<img class="img-rounded" alt="team-photo" src='pic/pe04.jpg' width="100%"> 
+    				   	
+    				   	<div class="team-member"></br>
+                        
+    				   	<h4>Jennifer</h4>
+    				   	
+    				   	<p>Admin</p>
+                        
+    				   	</div>
+    				   	
+    				   	<p class="social">
+    				   		<a href="#"><span class="fa fa-facebook-square"></span></a>
+    				   		<a href="#"><span class="fa fa-twitter-square"></span></a>
+    				   		<a href="#"><span class="fa fa-linkedin-square"></span></a>
+    				   		<a href="#"><span class="fa fa-google-plus-square"></span></a>
+    				   	</p>
+    						    					    				
+    		    </div> <!-- col-lg-4 -->
+                   
+    	</div>  <!-- row text-center -->
+    </div>
+</br></br>    
+
+    <div class="team">		
+            <div class="font05"><img width='10' height='10' src ='pic/icon03.png'></img> Roadmap</div>
+            <h1 class="font02">About Kalungka Roadmap</h1></br></br>
+            <img width='1250 px' src ='pic/pic04.png'></img>
+    </div>
+</div>
+<?php require_once __DIR__.'/Footer.php'; ?>	
     <!-- Login Modal -->
     <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
@@ -767,7 +1049,9 @@ div.single-input  {
                     </form>
                 </div>
             </div>
+        </div>    
             <!-- End Sign up -->
+
                 
 </body>
 
@@ -960,3 +1244,4 @@ function readURL(input) {
 });
 </script> -->
 </html>
+
